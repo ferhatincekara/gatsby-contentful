b8601c4ed4054a87192f285fc9d4f07b2c1515d0
ferhatincekara <ferhatincekara@gmail.com>
2021-12-31 20:20:44 +0300

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { Layout, Seo } from '@app/components';
+
 const NotFound: React.FC = () => {
-  return <div>Not Found</div>;
+  return (
+    <Layout>
+      <Seo title="Not Found" />
+
+      <section className="container pt-4 pb-10 md:pb-20 px-5 mx-auto">
+        <h3 className="text-h3 font-bold text-center">Not Found</h3>
+      </section>
+    </Layout>
+  );
 };
 
 export default NotFound;
