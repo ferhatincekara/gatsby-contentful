2d3a284887f7181b93bbe839b07cc6ccadc7813a
ferhatincekara <ferhatincekara@gmail.com>
2021-12-31 17:43:50 +0300

    Add Brands logos

---
+++ src/components/Brands/index.ts
@@ -0,0 +1,23 @@
+import Amazon from './Amazon';
+import Behance from './Behance';
+import Dribbble from './Dribbble';
+import Facebook from './Facebook';
+import Github from './Github';
+import Google from './Google';
+import Netlify from './Netlify';
+import Slack from './Slack';
+import Stripe from './Stripe';
+import Uber from './Uber';
+
+export default {
+  Amazon,
+  Behance,
+  Dribbble,
+  Facebook,
+  Github,
+  Google,
+  Netlify,
+  Slack,
+  Stripe,
+  Uber,
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+import Brands from './Brands';
 import Footer from './Footer';
 import Header from './Header';
 import LinkButton from './LinkButton';
 import Logo from './Logo';
 import Project from './Project';
 
-export { Footer, Header, LinkButton, Logo, Project };
+export { Brands, Footer, Header, LinkButton, Logo, Project };
